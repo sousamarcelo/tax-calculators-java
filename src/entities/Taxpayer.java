deadaa318124410a9131f74e5cb475060b7831f3
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-09-09 20:02:55 -0300

    Create abstrac class taxpayer

---
+++ src/entities/Taxpayer.java
@@ -0,0 +1,34 @@
+package entities;
+
+public abstract class Taxpayer {
+	private String name;
+	private Double annualIncome;
+	
+	public Taxpayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Taxpayer(String name, Double annualIncome) {
+		this.name = name;
+		this.annualIncome = annualIncome;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getAnnualIncome() {
+		return annualIncome;
+	}
+
+	public void setAnnualIncome(Double annualIncome) {
+		this.annualIncome = annualIncome;
+	}
+	
+	public abstract Double tax();	
+
+}
